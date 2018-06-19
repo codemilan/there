bb9ad97b2094c4926b5d6a38ce5e229af945fc0a
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-06-19 16:00:16 +0430

    Prepare for build

---
+++ vectors/Logo.js
@@ -0,0 +1,25 @@
+const LogoSvg = props => (
+  <svg width={34} height={34} viewBox="0 0 34 34" fill="none" {...props}>
+    <rect width={34} height={34} rx={6} fill="#74C5FF" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M11.285.003a1.453 1.453 0 0 0-1.537 1.36L9.22 9.86c-.007.112-.001.222.016.328a1.412 1.412 0 0 0 1.054 1.304l9.705 2.556c.762.2 1.548-.253 1.755-1.013a1.412 1.412 0 0 0-1.004-1.74l-8.57-2.257.466-7.503A1.446 1.446 0 0 0 11.285.003zM6.187 5.097c-.643-.072-1.231.455-1.315 1.178l-.786 6.805c-.008.07-.01.138-.01.205-.058.502.256 1.034.807 1.287l7.26 3.34c.668.308 1.43.09 1.703-.487.273-.576-.048-1.293-.716-1.6l-6.65-3.06.719-6.229c.083-.722-.37-1.367-1.012-1.44zM.959 10.113a.896.896 0 0 1 1.029-.733c.486.08.813.538.73 1.024l-.862 5.088 4.986 3.447a.87.87 0 0 1 .215 1.22.895.895 0 0 1-1.236.219L.408 16.635a.87.87 0 0 1-.306-.373.885.885 0 0 1-.09-.562l.947-5.587z"
+      transform="translate(6.649 6.467)"
+      fill="url(#paint0_linear)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear"
+        x2={1}
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="matrix(0 20.5352 -21.8009 0 21.8 0)"
+      >
+        <stop stopColor="#fff" />
+        <stop offset={1} stopColor="#fff" stopOpacity={0.93} />
+      </linearGradient>
+    </defs>
+  </svg>
+)
+
+export default LogoSvg

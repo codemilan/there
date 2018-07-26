c0fab49d5f37333df39477972292188aaf450d8b
Mohammad Rajabifard <mo.rajbi@gmail.com>
2018-07-26 22:05:04 +0430

    Fix flow errors for cors middleware

--- api/routes/cors.js
+++ api/routes/cors.js
@@ -1,10 +1,13 @@
 // @flow
 import cors from 'cors'
 
+const origin =
+  process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
+    ? ['https://there.pm', /there-(\w|-)+\.now\.sh/g, /there\.pm$/]
+    : [/localhost/]
+
 export default cors({
-  origin:
-    process.env.NODE_ENV === 'production' && !process.env.FORCE_DEV
-      ? ['https://there.pm', /there-(\w|-)+\.now\.sh/g, /there\.pm$/]
-      : [/localhost/],
+  // $FlowIssue
+  origin,
   credentials: true,
 })

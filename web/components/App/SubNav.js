93457bf99c8e735f57f34a2a2ea9b81c8f1b12d8
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-26 18:38:55 +0430

    Add subnav setting menuitems

--- web/components/App/SubNav.js
+++ web/components/App/SubNav.js
@@ -1,14 +1,22 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 // Local
 import { Container } from '../shared/Container'
 import Select from '../shared/Select'
 
-const SubNav = () => (
+const SubNav = ({ settingNav = false, ...props }) => (
   <Wrapper>
-    <Container>
-      <Select />
-    </Container>
+    <FlexContainer settingNav={settingNav}>
+      {!settingNav ? (
+        <Select />
+      ) : (
+        <SettingNav>
+          <Item href="/">Settings</Item>
+          <Item href="/">Members</Item>
+          <Item href="/">Billing</Item>
+        </SettingNav>
+      )}
+    </FlexContainer>
   </Wrapper>
 )
 
@@ -17,4 +25,29 @@ export default SubNav
 // Styles
 const Wrapper = styled.nav`
   padding: 4px 0;
+`
+
+const FlexContainer = styled(Container)`
+  ${p =>
+    p.settingNav &&
+    css`
+      display: flex;
+      justify-content: flex-end;
+    `};
+`
+
+const SettingNav = styled.div`
+  display: flex;
+  align-items: center;
+  height: 56px;
+`
+
+const Item = styled.a`
+  padding-left: 25px;
+
+  font-family: ${p => p.theme.fontTitle};
+  font-size: 15px;
+  text-decoration: none;
+
+  color: #97a5aa;
 `

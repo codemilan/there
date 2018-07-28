347d58b3858b4b8afd40babac91da0d2cf5a1da4
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-28 13:03:18 +0430

    Add Line components

---
+++ web/components/shared/Form/Line.js
@@ -0,0 +1,15 @@
+//@flow
+import React from 'react'
+import styled from 'styled-components'
+
+const Line = () => <LineWithStyle />
+
+const LineWithStyle = styled.div`
+  width: 100%;
+  height: 1px;
+  margin-top: 27px;
+  margin-bottom: 25px;
+  background: #ecf1f3;
+`
+
+export default Line

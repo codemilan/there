dd068bd4ec379af10555fbe553037b5e50668da6
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-30 21:00:29 +0430

    Add Error component

---
+++ web/components/shared/Form/Error.js
@@ -0,0 +1,10 @@
+//@flow
+import React from 'react'
+import styled from 'styled-components'
+
+// Local
+import Hint from './Hint'
+
+const Error = (props: any) => <Hint textColor="#f00">{props.children}</Hint>
+
+export default Error

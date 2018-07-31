d5435fbc1db5a7e2292fa81de5cec3b3930456ba
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-07-31 17:41:36 +0430

    Change Export Method

--- web/components/shared/Form/Line.js
+++ web/components/shared/Form/Line.js
@@ -1,15 +1,10 @@
-//@flow
-import React from 'react'
+// @flow
 import styled from 'styled-components'
 
-const Line = () => <LineWithStyle />
-
-const LineWithStyle = styled.div`
+export const Line = styled.div`
   width: 100%;
   height: 1px;
   margin-top: 27px;
   margin-bottom: 25px;
   background: #ecf1f3;
 `
-
-export default Line

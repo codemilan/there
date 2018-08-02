926cb6ff6eb36eef0a4224c13c4a265cbd729d88
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-08-02 09:00:12 +0430

    Add Row components

---
+++ web/components/App/TeamMembers/Row.js
@@ -0,0 +1,62 @@
+// @flow
+import * as React from 'react'
+import styled from 'styled-components'
+
+// Local
+import MemberPhoto from '../Person/MemberPhoto'
+import { BasicSelect } from '../../shared/Select'
+
+type Props = {
+  photoSrcSet: string,
+  name: string,
+  email: string,
+  onRoleChange: () => void,
+}
+
+const Row = ({ photoSrcSet, name, email, onRoleChange }: Props) => (
+  <tr>
+    <td>
+      <MemberItem>
+        <MemberPhoto
+          clickable={false}
+          size="small"
+          photoSrcSet={[photoSrcSet]}
+          status="working"
+          onClick={() => {}}
+          ring={false}
+        />
+        <div>
+          <MemberDisplayName>{name}</MemberDisplayName>
+          <Email>{email}</Email>
+        </div>
+      </MemberItem>
+    </td>
+    <td>
+      <BasicSelect onChange={onRoleChange} />
+    </td>
+  </tr>
+)
+
+export default Row
+
+// Styles
+const MemberDisplayName = styled.div`
+  line-height: 20px;
+  font-size: 15px;
+  letter-spacing: 0.01em;
+
+  color: black;
+`
+
+const Email = styled.div`
+  margin-top: 3px;
+  font-size: 13px;
+  letter-spacing: 0.02em;
+
+  color: #97a5aa;
+`
+
+const MemberItem = styled.div`
+  display: grid;
+  grid-template-columns: 45px 1fr;
+`
